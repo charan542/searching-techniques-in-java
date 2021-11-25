9b0c5589f2ee70784348745adcdb5de5e79fc4a0
charan542 <84331762+charan542@users.noreply.github.com>
2021-11-25 21:56:58 +0530

    Add files via upload

---
+++ New folder/uniqe.java
@@ -0,0 +1,16 @@
+public class uniqe{
+    public static void main(String args[]){
+        int a[]={1,1,4,7,8},i,flag=0,c=0,j,n;
+        n=a.length;
+        for(i=0;i<n;i++){
+            for(j=i+1;(j<n)&&(a[i]!=a[j]);j++){
+                
+                }if(j==n){
+                    c=c+1;
+
+            }
+
+        }
+        System.out.println(c);
+    }
+}
